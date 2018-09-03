e367081fb923b47568b5cc9ce15b1d45493a15a9
Hai Tran <tuanhai.dev@gmail.com>
2018-09-03 08:47:12 +0700

    Config and test Parse connection

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,8 +15,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    let configuration = ParseClientConfiguration {
+      $0.applicationId = "qjGrSbpjrBynl8StLaFJRxdb8HthHcRCOeE2F2LD"
+      $0.clientKey = "zzPHtr7w7JKAwgy9RYvZDm6bMzMRDuVU9YPJ3ckO"
+      $0.server = "https://parseapi.back4app.com/"
+    }
+    Parse.initialize(with: configuration)
+    saveInstallationObject()
+
     // Override point for customization after application launch.
     return true
+  }
+  func saveInstallationObject(){
+    if let installation = PFInstallation.current(){
+      installation.saveInBackground {
+        (success: Bool, error: Error?) in
+        if (success) {
+          print("You have successfully connected your app to Back4App!")
+        } else {
+          if let myError = error{
+            print(myError.localizedDescription)
+          }else{
+            print("Uknown error")
+          }
+        }
+      }
+    }
   }
 
   func applicationWillResignActive(_ application: UIApplication) {
