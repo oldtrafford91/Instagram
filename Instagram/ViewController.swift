cdf17fceb57bc9a88e78654f44d9e6c32b502b06
Hai Tran <tuanhai.dev@gmail.com>
2018-09-03 08:09:26 +0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Tran Tuan Hai on 9/3/18.
+//  Copyright © 2018 Tran Tuan Hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
