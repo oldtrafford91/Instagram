6095061b229c7b9cf90590131b4356640b2279c9
Hai Tran <tuanhai.dev@gmail.com>
2018-09-04 09:59:27 +0700

    Clean up

--- Instagram/ForgotPasswordViewController.swift
+++ Instagram/ForgotPasswordViewController.swift
@@ -1,30 +1,10 @@
-//
-//  ForgotPasswordViewController.swift
-//  Instagram
-//
-//  Created by Tran Tuan Hai on 9/4/18.
-//  Copyright © 2018 Tran Tuan Hai. All rights reserved.
-//
-
 import UIKit
 
 class ForgotPasswordViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+  }
 }
--- Instagram/SignUpViewController.swift
+++ Instagram/SignUpViewController.swift
@@ -1,30 +1,9 @@
-//
-//  SignUpViewController.swift
-//  Instagram
-//
-//  Created by Tran Tuan Hai on 9/4/18.
-//  Copyright © 2018 Tran Tuan Hai. All rights reserved.
-//
-
 import UIKit
 
 class SignUpViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
 }
